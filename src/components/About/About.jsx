3e37a8113d5cdea4f8437527a20b33d0366d5822
Natasha Coen <nscoen19@gmail.com>
2022-03-19 18:18:51 -0700

    Style About mobile page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,30 +8,51 @@ const About = () => {
     <div id="about-page">
       <Header />
       <PageTitle title="ABOUT US" color="#3D4D43" />
-
-      <p id="about-title1">A LOCAL COMPANY</p>
-      <p id="about-title2">A MORE RESPONSIVE COMPANY</p>
-      <img
-        src="https://res.cloudinary.com/tca7lsohuid8/image/upload/v1642563108/lcr_website/Office_Manager_with_Border_w1dz2y.png"
-        alt="office manager"
-        width="300px"
-      />
-      <img
-        src="https://res.cloudinary.com/tca7lsohuid8/image/upload/v1642563102/lcr_website/Happy_Work_Force_with_Border_ycpuuq.png"
-        alt="work force"
-        width="300px"
-      />
-      <div id="leased">
-        <p id="about-p1">
+      <div className="about-title">
+        <p id="title1">A LOCAL COMPANY</p>
+        <p id="title2">A MORE RESPONSIVE COMPANY</p>
+      </div>
+      <div className="worker-imgs">
+        <img
+          id="office-manager"
+          src="https://res.cloudinary.com/tca7lsohuid8/image/upload/v1642563108/lcr_website/Office_Manager_with_Border_w1dz2y.png"
+          alt="office manager"
+          width="300px"
+        />
+        <img
+          id="work-force"
+          src="https://res.cloudinary.com/tca7lsohuid8/image/upload/v1642563102/lcr_website/Happy_Work_Force_with_Border_ycpuuq.png"
+          alt="work force"
+          width="300px"
+        />
+      </div>
+      <div>
+        <p className="about-paragraph" id="about-p1">
           SINCE 2008, WE HAVE SERVED THE GREATER BAY <br /> AREA AND ITS
           SURROUNDING COMMUNITIES.
         </p>
-        <p id="about-p2">
+        <p className="about-paragraph" id="about-p2">
           WE SPECIALIZE IN COMMERCIAL REMODELING BOTH INTERIOR AND EXTERIOR,
           RETAIL LOCATION, AND REGULATORY COMPLIANCE WITH LOCAL JURISDICTIONS.
         </p>
-        <div className="about-pics" id="office-worker"></div>
-        <div className="about-pics" id="workers"></div>
+      </div>
+      <div className="leased">
+        <img
+          id="nc-badge"
+          src="https://res.cloudinary.com/tca7lsohuid8/image/upload/v1642563087/lcr_website/North_Coast_Builders_Exchange_kicswb.png"
+          alt="North Coast Builders Exchange Badge"
+        />
+        <img
+          id="marin-badge"
+          src="https://res.cloudinary.com/tca7lsohuid8/image/upload/v1642563081/lcr_website/Marin_Builders_Badge_k83d3s.png"
+          alt="Marin Builders Badge"
+        />
+        <img
+          id="building"
+          src="https://res.cloudinary.com/tca7lsohuid8/image/upload/v1642306845/lcr_website/Leased_wfvpo6.png"
+          alt="leased building"
+          width="300px"
+        />
       </div>
     </div>
   );
